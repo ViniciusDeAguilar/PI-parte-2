c44d3a5fd7f04d37410b9aa87dd1bda105bb131d
Vinícius de Aguilar Amorim <90862709+ViniciusDeAguilar@users.noreply.github.com>
2022-12-06 15:25:29 -0300

    Update jogadorModel.js

--- src/models/jogadorModel.js
+++ src/models/jogadorModel.js
@@ -7,65 +7,16 @@ const jogadorSchema = mongoose.Schema({
         maxLength: [20, 'O nome do jogador deve no máximo 20 caracteres'],
         required: [ true, 'Nome do jogador é um parâmetro obrigatório!']
     },
-    nomePersonagem: {
+    telefone: {
         type: String,
         trim: true,
-        maxLength: [80 , 'O nome do personagem deve no máximo 80 caracteres'],
-        required: [ true, 'Nome do jogador é um parâmetro obrigatório!']
+        maxLenght: [15, 'O telefone deve ter menos de 15 caracteres']
     },
-    raca: {
+    email: {
         type: String,
         trim: true,
-        enum: {
-            values: ['Orc', 'Humano', 'Elfo', 'Meio-Orc', 'Meio-Elfo', 'Anão', 'Gnomo', 'Dragonborn', 'Tiefling', 'Halfling'],
-            message: '{VALUE} não é uma raça disponível no livro do jogador de D&D 5e'
-        },
-        required: [ true, 'Raça é um parâmetro obrigatório']
-    },
-    classe: {
-        type: String,
-        trim: true,
-        enum: {
-            values: ['Bárbaro', 'Bardo', 'Clérigo', 'Druida', 'Guerreiro', 'Monge', 'Paladino', 'Caçador', 'Ladino', 'Feiticeiro', 'Bruxo', 'Mago'],
-            message: '{VALUE} não é uma classe disponível no livro do jogador de D&D 5e'
-        },
-        required: [ true, 'Classe é um parâmetro obrigatório']
-    },
-    forca: {
-        type: Number,
-        min: [0, 'Um atributo não pode ser menor que 0!'],
-        required: [ true, 'Um atributo não pode ser nulo']
-    },
-    constituicao: {
-        type: Number,
-        min: [0, 'Um atributo não pode ser menor que 0!'],
-        required: [ true, 'Um atributo não pode ser nulo']
-    },
-    dextreza: {
-        type: Number,
-        min: [0, 'Um atributo não pode ser menor que 0!'],
-        required: [ true, 'Um atributo não pode ser nulo']
-    },
-    inteligencia: {
-        type: Number,
-        min: [0, 'Um atributo não pode ser menor que 0!'],
-        required: [ true, 'Um atributo não pode ser nulo']
-    },
-    carisma: {
-        type: Number,
-        min: [0, 'Um atributo não pode ser menor que 0!'],
-        required: [ true, 'Um atributo não pode ser nulo']
-    },
-    sabedoria: {
-        type: Number,
-        min: [0, 'Um atributo não pode ser menor que 0!'],
-        required: [ true, 'Um atributo não pode ser nulo']
-    },
-    jogando: {
-        type: Boolean,
-        required: [ true, 'Nossa API não está preparada para jogadores de Schrodinger!']
+        maxLenght: [50, 'O telefone deve ter menos de 50 caracteres']
     }
-    
 });
 
-module.exports = mongoose.model('Ficha', fichaSchema);
+module.exports = mongoose.model('Jogador', jogadorSchema);
